4eb8c1f1fd13ed811ac5b1edd6e1f2fd57382c9f
Emily Behrendsen <emilyanne938@gmail.com>
2023-11-06 13:53:34 -0500

    wrote main, connection and create server functions

--- pongServer.py
+++ pongServer.py
@@ -7,7 +7,7 @@
 # =================================================================================================
 
 import socket
-import threading
+from threading import Thread
 
 # Use this file to write your server logic
 # You will need to support at least two clients
@@ -16,43 +16,129 @@ import threading
 # I suggest you use the sync variable in pongClient.py to determine how out of sync your two
 # clients are and take actions to resync the games
 
-# Create a socket
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+quit = True
 
-serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+def createServer():
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+    serverSocket.bind(("localhost", 22222))
+    serverSocket.listen()
+    clientSocket, clientAddress = serverSocket.accept()
+    return serverSocket
 
-# Bind the socket to a specific IP address and port
-serverSocket.bind(("localhost", 8888))
+def connection(serverSocket, clientSocket):
+    msg = ""
+    screenWidth = 640
+    screenHeight = 480
+    numClients = 0
 
-# Put the server socket in a listening state 
-serverSocket.listen(5)
+    if numClients % 2 == 1:
+        playerPaddle = "left"
+    else:
+        playerPaddle = "right"
 
-clientSocket, clientAddress = serverSocket.accept()
+    check = clientSocket.recv(1024).decode()
 
-numClients = 0
+    serverSocket.send(screenHeight)
 
-msg = ""
-msg = clientSocket.recv(1024).decode()
-print("Client sent: {msg}")
+    if check == "height_res":
+        serverSocket.send(screenWidth)
 
-screenWidth = 640
-screenHeight = 480
+    check = clientSocket.recv(1024).decode()
 
-if numClients % 2 == 1:
-    playerPaddle = "left"
-else:
-    playerPaddle = "right"
-
-check = msg.recv(1024).decode
-
-serverSocket.send(screenHeight)
-
-if check == "height_ack":
-    serverSocket.send(screenWidth)
-
-if check == "width_ack":
-    serverSocket.send(playerPaddle)
+    if check == "width_res":
+        serverSocket.send(playerPaddle)
 
 
-clientSocket.close()
-serverSocket.close()
+
+def main():
+    serverSocket = createServer()
+
+
+    # 
+    serverSocket.recv
+    # connection(serverSocket)
+    
+
+    maxplayers = 2
+    currentNumClients = 0
+    
+    clientSocket, clientAddress = serverSocket.accept()
+    # args = (serverSocket, clientSocket,)
+
+    while not quit:
+
+        clientSocket, clientAddress = serverSocket.accept()
+        msg = clientSocket.recv(1024).decode()
+        print(msg)
+
+        if (msg == "Can I play?") & (currentNumClients <= maxplayers):
+
+            print("did we get here")
+            msg == ""
+            # make treads
+            thread = Thread(target = connection, args = (serverSocket, clientSocket))
+            print(currentNumClients)
+            currentNumClients += 1
+            print("how about here")
+            thread.start()
+
+
+    serverSocket.close()
+
+
+
+
+if __name__ == "__main__" :
+    main()
+
+    
+    # accept new clients
+    # create threads
+
+
+  
+
+
+
+# # Create a socket
+# serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# # serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+# # serverIP ="localhost"
+# # serverPort = 8888
+# # Bind the socket to a specific IP address and port
+# serverSocket.bind(("localhost", 22222))
+
+# # Put the server socket in a listening state 
+# serverSocket.listen(5)
+
+# clientSocket, clientAddress = serverSocket.accept()
+
+# numClients = 0
+
+# msg = ""
+# # msg = clientSocket.recv(1024).decode()
+# # print("Client sent: {msg}")
+
+# screenWidth = 640
+# screenHeight = 480
+
+# if numClients % 2 == 1:
+#     playerPaddle = "left"
+# else:
+#     playerPaddle = "right"
+
+# check = msg.recv(1024).decode
+
+# serverSocket.send(screenHeight)
+
+# if check == "height_ack":
+#     serverSocket.send(screenWidth)
+
+# if check == "width_ack":
+#     serverSocket.send(playerPaddle)
+
+
+# # clientSocket.close()
+# # serverSocket.close()
